2ffffb32085ed68b5ef6e2df8c632d59c65d12d7
pinkmouse <justin.mariette@gmail.com>
2015-01-15 11:31:50 -0600

    some missing files, learning git command

--- website/src/app/index.js
+++ website/src/app/index.js
@@ -4,7 +4,7 @@ angular.module('sheaker', ['ngResource', 'ngRoute', 'ui.bootstrap'])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'app/main/main.html',
+        templateUrl: 'app/home/home.html',
         controller: 'MainCtrl'
       })
       .otherwise({
