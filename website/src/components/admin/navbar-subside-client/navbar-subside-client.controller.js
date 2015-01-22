5c618bf74db445b83f7c923add4beae9c0e18bf3
pinkmouse <justin.mariette@gmail.com>
2015-01-22 12:37:47 -0600

    Website: Change sidebar

--- website/src/components/admin/navbar-subside-client/navbar-subside-client.controller.js
+++ website/src/components/admin/navbar-subside-client/navbar-subside-client.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('sheaker')
-  .controller('NavbarSideCtrl', function ($scope, $location) {
+  .controller('NavbarSubSideClientCtrl', function ($scope, $location) {
       $scope.isActive = function(path) {
           if ($location.path() === path) {
               return true;
