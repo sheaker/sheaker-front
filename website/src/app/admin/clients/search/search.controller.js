2be6c29658aec5dbaca34676001a66ae68d89b80
Tuxity <kevin.darcel@gmail.com>
2015-02-19 16:16:17 -0600

    Website: Begin to do search of a client

--- website/src/app/admin/clients/search/search.controller.js
+++ website/src/app/admin/clients/search/search.controller.js
@@ -1,52 +1,17 @@
 'use strict';
 
 angular.module('sheaker')
-.controller('SearchClientCtrl', function ($scope, $location) {
+.controller('SearchClientCtrl', function ($scope, $location, User) {
 
     if ($location.search().search) {
         $scope.searchText = $location.search().search;
     }
 
     $scope.$watch('searchText', function(newValue) {
-        $location.search('search', newValue);
-    });
+        $location.search('text', newValue);
 
-    $scope.clientlist= [
-    {
-        'id': '1',
-        'name': 'Justin',
-        'lastname': 'Mariette',
-        'subscription': 'true',
-    },
-    {
-        'id': '2',
-        'name': 'Paul',
-        'lastname': 'Lulu',
-        'subscription': 'true',
-    },
-    {
-        'id': '3',
-        'name': 'Plop',
-        'lastname': 'Mama',
-        'subscription': 'true',
-    },
-    {
-        'id': '4',
-        'name': 'Mamadous',
-        'lastname': 'Coulibali',
-        'subscription': 'true',
-    },
-    {
-        'id': '5',
-        'name': 'Jusdd',
-        'lastname': 'Ted',
-        'subscription': 'false',
-    },
-    {
-        'id': '6',
-        'name': 'Frank',
-        'lastname': 'bite',
-        'subscription': 'true',
-    }
-    ];
+        if (newValue && newValue.length > 3) {
+            $scope.clientlist = User.query({'text': newValue});
+        }
+    });
 });
--- website/src/components/user/user.factory.js
+++ website/src/components/user/user.factory.js
@@ -9,6 +9,11 @@ angular.module('sheaker')
             method: 'POST',
             url: API_URL + '/users/login',
             skipAuthorization: true
+        },
+        search: {
+            method: 'GET',
+            url: API_URL + '/users/search',
+            skipAuthorization: true
         }
     };
 
