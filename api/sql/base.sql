0b2aa7c2c0b86cd0c2a5d2ce5d1862543a01f1be
Tuxity <kevin.darcel@gmail.com>
2015-02-23 17:14:06 -0600

    API: Dump sql database

--- api/sql/base.sql
+++ api/sql/base.sql
@@ -5,9 +5,9 @@
 # http://www.sequelpro.com/
 # http://code.google.com/p/sequel-pro/
 #
-# Hôte: localhost (MySQL 10.0.15-MariaDB)
+# Hôte: localhost (MySQL 10.0.16-MariaDB)
 # Base de données: gymname
-# Temps de génération: 2015-01-31 02:43:47 +0000
+# Temps de génération: 2015-02-23 23:12:59 +0000
 # ************************************************************
 
 
@@ -40,7 +40,7 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`first_name`),
   UNIQUE KEY `email` (`mail`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
@@ -49,7 +49,8 @@ INSERT INTO `users` (`id`, `first_name`, `last_name`, `password`, `mail`, `birth
 VALUES
 	(1,'user','','$2y$10$AHlCzRePTRmzTKmb.WbZROznKmHGCpnmjErwfs2NngcYHk1lnVqOG','user@user.user','2015-01-16 10:13:20',NULL,'2015-01-22 19:26:31','http://localhos','2015-01-02 18:43:59',0),
 	(2,'modo','','$2y$10$l9IQju8HMi30izZukfkml.ecV/fjLFs5wsEV/gVIiAU.CBfFAh3yK','modo@modo.modo','2015-01-16 10:13:20',NULL,'0000-00-00 00:00:00','127.0.0.1','2015-01-02 18:44:16',0),
-	(3,'admin','','$2y$10$sf1krqI89gRZQLo6L5E4aeTqaxa3DXCaZFKaBJ3/Xj2NlpfaZAOxq','admin@admin.admin','2015-01-16 10:13:20',NULL,'2015-01-25 03:25:54','http://127.0.0.','2015-01-02 18:44:32',0);
+	(3,'admin','','$2y$10$sf1krqI89gRZQLo6L5E4aeTqaxa3DXCaZFKaBJ3/Xj2NlpfaZAOxq','admin@admin.admin','2015-01-16 10:13:20',NULL,'2015-01-25 03:25:54','http://127.0.0.','2015-01-02 18:44:32',0),
+	(4,'kevin','darcel','$2y$10$daUTTc4yiSZ2Nn2GJt./UeU/51CydHrdYjOTURLkGhREQS1OeZS4q','kevin.darcel@gmail.com','1990-08-01 08:00:00',0,NULL,'','2015-02-21 16:32:55',0);
 
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -79,18 +80,18 @@ VALUES
 UNLOCK TABLES;
 
 
-# Affichage de la table users_avatar
+# Affichage de la table users_photo
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `users_avatar`;
+DROP TABLE IF EXISTS `users_photo`;
 
-CREATE TABLE `users_avatar` (
+CREATE TABLE `users_photo` (
   `user_id` int(11) unsigned NOT NULL,
   `image` blob NOT NULL,
   `name` varchar(255) NOT NULL DEFAULT '',
   `added_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`user_id`),
-  CONSTRAINT `users_avatar_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+  CONSTRAINT `users_photo_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
