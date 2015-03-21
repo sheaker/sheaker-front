4fb9d145521f8cb8a23ef9b8beca4b4b48a8c6d4
Tuxity <kevin.darcel@gmail.com>
2015-03-20 19:39:22 -0600

    Website: update gulp files to 0.10.0 of Swiip/generator-gulp-angular

--- website/gulp/unit-tests.js
+++ website/gulp/unit-tests.js
@@ -5,27 +5,58 @@ var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 
 var wiredep = require('wiredep');
+var karma = require('karma');
+var concat = require('concat-stream');
+var _ = require('lodash');
 
 module.exports = function(options) {
-  function runTests (singleRun) {
+  function listFiles(callback) {
     var bowerDeps = wiredep({
       directory: 'bower_components',
-      exclude: ['bootstrap-sass-official'],
+      exclude: [/bootstrap\.js/],
       dependencies: true,
       devDependencies: true
     });
 
-    var testFiles = bowerDeps.js.concat([
-      options.src + '/{app,components}/**/*.js'
-    ]);
+    var specFiles = [
+      options.src + '/**/*.spec.js',
+      options.src + '/**/*.mock.js'
+    ];
 
-    return gulp.src(testFiles)
-      .pipe($.karma({
-        configFile: 'karma.conf.js',
-        action: (singleRun)? 'run': 'watch'
+    var htmlFiles = [
+      options.src + '/**/*.html'
+    ];
+
+    var srcFiles = [
+      options.src + '/{app,components}/**/*.js'
+    ].concat(specFiles.map(function(file) {
+      return '!' + file;
+    }));
+
+
+    gulp.src(srcFiles)
+      .pipe(concat(function(files) {
+        callback(bowerDeps.js
+          .concat(_.pluck(files, 'path'))
+          .concat(htmlFiles)
+          .concat(specFiles));
       }));
   }
 
-  gulp.task('test', ['scripts'], runTests.bind(this, true));
-  gulp.task('test:auto', ['scripts'], runTests.bind(this, false));
+  function runTests (singleRun, done) {
+    listFiles(function(files) {
+      karma.server.start({
+        configFile: __dirname + '/../karma.conf.js',
+        files: files,
+        singleRun: singleRun
+      }, done);
+    });
+  }
+
+  gulp.task('test', ['scripts'], function(done) {
+    runTests(true, done);
+  });
+  gulp.task('test:auto', ['watch'], function(done) {
+    runTests(false, done);
+  });
 };
