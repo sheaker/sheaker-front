2e79025125f4f68b7b83f2f0cf2184259c0f20b9
Tuxity <kevin.darcel@gmail.com>
2015-04-03 09:36:06 -0600

    Redump properly sql files

--- api/sql/base.sql
+++ api/sql/base.sql
@@ -1,7 +1,7 @@
 #
 # SQL Export
 # Created by Querious (962)
-# Created: April 3, 2015 at 9:01:56 AM CST
+# Created: April 3, 2015 at 9:32:29 AM CST
 # Encoding: Unicode (UTF-8)
 #
 
@@ -13,26 +13,27 @@ DROP TABLE IF EXISTS `users`;
 
 CREATE TABLE `users` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `custom_id` int(11) unsigned DEFAULT NULL,
+  `custom_id` int(11) unsigned DEFAULT '0',
   `first_name` varchar(255) NOT NULL DEFAULT '',
   `last_name` varchar(255) NOT NULL DEFAULT '',
   `password` varchar(255) NOT NULL,
-  `phone` varchar(128) DEFAULT NULL,
+  `phone` varchar(128) DEFAULT '',
   `mail` varchar(255) DEFAULT '',
-  `birthdate` date DEFAULT NULL,
+  `birthdate` date DEFAULT '0000-00-00',
   `address_street_1` varchar(255) DEFAULT '',
-  `address_street_2` varchar(255) DEFAULT NULL,
-  `city` varchar(255) DEFAULT NULL,
-  `zip` int(11) unsigned DEFAULT NULL,
+  `address_street_2` varchar(255) DEFAULT '',
+  `city` varchar(255) DEFAULT '',
+  `zip` varchar(10) DEFAULT '',
   `gender` tinyint(1) DEFAULT '-1',
-  `sponsor_id` int(11) unsigned DEFAULT NULL,
+  `photo` varchar(255) DEFAULT '',
+  `sponsor_id` int(11) unsigned DEFAULT '0',
   `comment` text,
   `failed_logins` int(11) unsigned NOT NULL DEFAULT '0',
   `last_seen` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `last_ip` varchar(255) NOT NULL DEFAULT '0.0.0.0',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 CREATE TABLE `users_access` (
@@ -40,13 +41,13 @@ CREATE TABLE `users_access` (
   `user_level` tinyint(1) unsigned NOT NULL,
   PRIMARY KEY (`user_id`),
   KEY `idx_user_id` (`user_id`) USING BTREE,
-  FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+  FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 CREATE TABLE `users_payments` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) unsigned NOT NULL DEFAULT '0',
+  `user_id` int(11) unsigned DEFAULT '0',
   `days` smallint(5) NOT NULL DEFAULT '-1',
   `start_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `end_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
@@ -56,40 +57,9 @@ CREATE TABLE `users_payments` (
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `idx_user_id` (`user_id`) USING BTREE,
-  FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+  FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE SET NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
-
-
-SET @PREVIOUS_FOREIGN_KEY_CHECKS = @@FOREIGN_KEY_CHECKS;
-SET FOREIGN_KEY_CHECKS = 0;
-
-
-LOCK TABLES `users` WRITE;
-ALTER TABLE `users` DISABLE KEYS;
-INSERT INTO `users` (`id`, `custom_id`, `first_name`, `last_name`, `password`, `phone`, `mail`, `birthdate`, `address_street_1`, `address_street_2`, `city`, `zip`, `gender`, `sponsor_id`, `comment`, `failed_logins`, `last_seen`, `last_ip`, `created_at`) VALUES 
-	(1,0,'admin','admin','$1$OhuHu9HV$MGnsyEEGXt6Y6QShwL7ZM/','523312345678','admin@sheaker.com','0000-00-00','','','',0,0,0,'',0,'2015-03-19 16:27:51','127.0.0.1','2015-01-01 00:00:00');
-ALTER TABLE `users` ENABLE KEYS;
-UNLOCK TABLES;
-
-
-LOCK TABLES `users_access` WRITE;
-ALTER TABLE `users_access` DISABLE KEYS;
-INSERT INTO `users_access` (`user_id`, `user_level`) VALUES 
-	(1,2);
-ALTER TABLE `users_access` ENABLE KEYS;
-UNLOCK TABLES;
-
-
-LOCK TABLES `users_payments` WRITE;
-ALTER TABLE `users_payments` DISABLE KEYS;
-ALTER TABLE `users_payments` ENABLE KEYS;
-UNLOCK TABLES;
-
-
-
-
-SET FOREIGN_KEY_CHECKS = @PREVIOUS_FOREIGN_KEY_CHECKS;
 
 
