6663a16fa1af47774c5bceb12c638cdaaa0b3660
Tuxity <kevin.darcel@gmail.com>
2015-04-16 12:54:39 -0500

    Force route reload when loginout

--- src/app/logout/logout.controller.js
+++ src/app/logout/logout.controller.js
@@ -1,7 +1,8 @@
 'use strict';
 
 angular.module('sheaker')
-.controller('LogoutCtrl', function ($window, $location) {
+.controller('LogoutCtrl', function ($window, $location, $route) {
     $window.localStorage.removeItem('token');
     $location.path('/');
+    $route.reload();
 });
