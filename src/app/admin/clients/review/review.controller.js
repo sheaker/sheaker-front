023b65b807c52cef890bdf8e4fe077a7096bc346
pinkmouse <justin.mariette@gmail.com>
2015-05-15 15:04:49 -0500

    Test review

--- src/app/admin/clients/review/review.controller.js
+++ src/app/admin/clients/review/review.controller.js
@@ -1,11 +1,51 @@
 'use strict';
 
 angular.module('sheaker')
-.controller('ReviewClientCtrl', function ($rootScope, $scope, $routeParams, $location) {
+.controller('ReviewClientCtrl', function ($rootScope, $scope, $routeParams, $location, User, Payment, Checkin, GYM_API_URL) {
 
     if (typeof $routeParams.id === 'undefined') {
         $rootScope.alerts.push({type: 'warning', msg: 'Please search a user to review before going to this page.'});
         $location.path('/admin/clients/search');
     }
+
+    $scope.formDatas = {};
+    $scope.lastCheckins = [];
+
+    User.get({id: $routeParams.id}, function(user) {
+        user.photo = '//static.sheaker.com/sheaker-gym/assets/images/user_unknow.png';
+        if ($scope.formDatas.photo) {
+            user.photo = GYM_API_URL + '/' + user.photo;
+        }
+
+        Payment.query({user: user.id}).$promise
+        .then(function(payments) {
+            user.payments = payments;
+        })
+        .catch(function(error) {
+            console.log(error);
+            $rootScope.alerts.push({type: 'danger', msg: 'An error happen while retrieving user payments.'});
+        });
+        $scope.user = user;
+
+        if (user.lastCheckins) {
+            user.lastCheckins.forEach(function (checkin) {
+                var userObject = {
+                    user: {
+                        id: user.id,
+                        customId: user.customId,
+                        firstName: user.firstName,
+                        lastName: user.lastName
+                    }
+                };
+                checkin = angular.extend(checkin, userObject);
+            });
+            $scope.lastCheckins = $scope.lastCheckins.concat(user.lastCheckins);
+        }
+
+    }, function(error) {
+        console.log(error);
+        $rootScope.alerts.push({type: 'danger', msg: 'Error while retriving the user informations.'});
+        $location.path('/admin/clients/search');
+    });
 
 });
