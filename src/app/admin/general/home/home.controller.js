fe7dd4e3fd630a3658696888f992048d15badd79
Tuxity <kevin.darcel@gmail.com>
2015-08-13 16:47:51 -0500

    Correctly retrieve the number of days paid for new membership

--- src/app/admin/general/home/home.controller.js
+++ src/app/admin/general/home/home.controller.js
@@ -54,49 +54,23 @@ angular.module('sheaker')
     // Retrieve new memberships
     Payment.statsNew().$promise
     .then(function (memberships) {
+        memberships.forEach(function (user) {
+            Payment.get({payment_id: user.active_membership_id}).$promise
+            .then(function(payment) {
+                user.days = payment.days;
+            })
+            .catch(function(error) {
+                console.log(error);
+                $rootScope.alerts.push({type: 'danger', msg: 'An error happen while retrieving payment.', exp: 5000});
+            });
+        });
+
         $scope.newMemberships = memberships;
     })
     .catch(function(error) {
         console.log(error);
         $rootScope.alerts.push({type: 'danger', msg: 'Error while retrieving the new memberships.'});
     });
-
-    /*
-    User.query().$promise
-    .then(function(usersList) {
-        usersList.forEach(function (user) {
-            // Calculate if his birthday is in 3 days
-            // Add years between birthdate and now to calculate next anniversary
-            var furtherBirthdate = moment(user.birthdate).add(today.diff(user.birthdate, 'years') + 1, 'years');
-            user.hasBirthdayInc = furtherBirthdate.isBetween(today, statsToFurtherDate, 'second');
-            if (user.hasBirthdayInc) {
-                user.furtherBirthdate = furtherBirthdate;
-            }
-
-            // Calculate if his membership (for users which have one) will expire in 3 days
-            user.hasInactiveMembershipInc = false;
-            if (user.activeMembershipId) {
-                user.hasInactiveMembershipInc = moment(user.activeMembership.endDate).isBetween(today, statsToFurtherDate, 'second');
-            }
-
-            // Calculate if it's a recent membership (for users which have one)
-            user.hasNewActiveMembership = false;
-            if (user.activeMembershipId) {
-                user.hasNewActiveMembership = moment(user.activeMembership.paymentDate).isBetween(statsToPreviousDate, now, 'second');
-            }
-
-            if (user.userLevel !== 0) {
-                $scope.staffMember[user.userLevel] += 1;
-            }
-        });
-
-        $scope.usersList = usersList;
-    })
-    .catch(function(error) {
-        console.log(error);
-        $rootScope.alerts.push({type: 'danger', msg: 'Error while retrieving the users.'});
-    });
-    */
 });
 
 })();
