dddde91a021ee62d776c991f3a6cd865dc5ae80c
Tuxity <kevin.darcel@gmail.com>
2015-08-30 13:29:21 -0500

    Home: use moment to get dates

--- src/app/admin/general/home/home.controller.js
+++ src/app/admin/general/home/home.controller.js
@@ -7,7 +7,10 @@
 
     function HomeAdminCtrl($rootScope, $scope, $location, User, Payment, Checkin) {
 
-        $scope.staffMember = [0, 0, 0, 0];
+        var todayDate        = moment().format('DD/MM/YYYY'),
+            yesterdayDate    = moment().subtract(1, 'day').format('DD/MM/YYYY'),
+            startOfWeekDate  = moment().startOf('week').format('DD/MM/YYYY'),
+            startOfMonthDate = moment().startOf('month').format('DD/MM/YYYY');
 
         $scope.users = {
             active: 0,
@@ -25,7 +28,7 @@
             check: 0
         };
 
-        Payment.getGainsFromDate({from_date: '24/08/2015', to_date: '24/08/2015'}).$promise
+        Payment.getGainsFromDate({from_date: todayDate}).$promise
         .then(function (gains) {
             $scope.gains.today = gains.total.value;
         })
@@ -34,7 +37,7 @@
             $rootScope.alerts.push({type: 'danger', msg: 'Error while retrieving gains of today.'});
         });
 
-        Payment.getGainsFromDate({from_date: '31/07/2015', to_date: '31/07/2015'}).$promise
+        Payment.getGainsFromDate({from_date: yesterdayDate, to_date: yesterdayDate}).$promise
         .then(function (gains) {
             $scope.gains.yesterday = gains.total.value;
         })
@@ -43,7 +46,7 @@
             $rootScope.alerts.push({type: 'danger', msg: 'Error while retrieving gains of yesterday.'});
         });
 
-        Payment.getGainsFromDate({from_date: '25/07/2015', to_date: '01/08/2015'}).$promise
+        Payment.getGainsFromDate({from_date: startOfWeekDate}).$promise
         .then(function (gains) {
             $scope.gains.week = gains.total.value;
         })
@@ -52,7 +55,7 @@
             $rootScope.alerts.push({type: 'danger', msg: 'Error while retrieving gains of this week.'});
         });
 
-        Payment.getGainsFromDate({from_date: '01/07/2015', to_date: '31/08/2015'}).$promise
+        Payment.getGainsFromDate({from_date: startOfMonthDate}).$promise
         .then(function (gains) {
             $scope.gains.month = gains.total.value;
         })
