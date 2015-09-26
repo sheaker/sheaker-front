7c281888f5a51b4d88026aef3c086e6137500175
Tuxity <kevin.darcel@gmail.com>
2015-09-26 15:39:07 -0500

    Graph: Begin selectors for graphs to allow user to change from_date, to_date and the interval

--- src/app/admin/statistics/gym/gym.controller.js
+++ src/app/admin/statistics/gym/gym.controller.js
@@ -6,7 +6,6 @@
         .controller('GymStatisticsCtrl', GymStatisticsCtrl);
 
     function GymStatisticsCtrl($rootScope, $scope, Payment, User) {
-        var startOfYear = moment().startOf('year').format();
 
         $scope.gains = {
             labels: {},
@@ -18,25 +17,64 @@
             data: {}
         };
 
-        Payment.getGainsGraph({interval: 'month', from_date: startOfYear}).$promise
-        .then(function(response) {
-            $scope.gains.labels = response.labels;
-            $scope.gains.data = response.data;
-        })
-        .catch(function(error) {
-            console.log(error);
-            $rootScope.alertsMsg.error('Error while retrieving gains graphs.');
+        $scope.availableIntervals = ['year', 'quarter', 'month', 'week', 'day'];
+
+        $scope.fromDate = moment().startOf('year').format();
+        $scope.toDate = moment().format();
+        $scope.interval = 'month';
+        $scope.fromDateSelector = {
+            endOfYesterday: moment().subtract(1, 'day').endOf('day').format(),
+            isOpen: false,
+            openCal: function($event) {
+                $event.preventDefault();
+                $event.stopPropagation();
+                $scope.fromDateSelector.isOpen = true;
+            },
+            dateOptions: {
+                startingDay: 1
+            }
+        };
+        $scope.toDateSelector = {
+            endOfYesterday: moment().subtract(1, 'day').endOf('day').format(),
+            isOpen: false,
+            openCal: function($event) {
+                $event.preventDefault();
+                $event.stopPropagation();
+                $scope.toDateSelector.isOpen = true;
+            },
+            dateOptions: {
+                startingDay: 1
+            }
+        };
+
+        $scope.$watchGroup(['fromDate','toDate', 'interval'], function(newValue) {
+                getGains();
+                getNewUsers();
         });
 
-        User.getNewUsers({interval: 'month', from_date: startOfYear}).$promise
-        .then(function(response) {
-            $scope.newUsers.labels = response.labels;
-            $scope.newUsers.data = response.data;
-        })
-        .catch(function(error) {
-            console.log(error);
-            $rootScope.alertsMsg.error('Error while retrieving new clients graphs.');
-        });
+        function getGains() {
+            Payment.getGainsGraph({from_date: $scope.fromDate, to_date: $scope.toDate, interval: $scope.interval}).$promise
+                .then(function(response) {
+                    $scope.gains.labels = response.labels;
+                    $scope.gains.data = response.data;
+                })
+                .catch(function(error) {
+                    console.log(error);
+                    $rootScope.alertsMsg.error('Error while retrieving gains graphs.');
+                });
+        }
+
+        function getNewUsers() {
+            User.getNewUsers({from_date: $scope.fromDate, to_date: $scope.toDate, interval: $scope.interval}).$promise
+                .then(function(response) {
+                    $scope.newUsers.labels = response.labels;
+                    $scope.newUsers.data = response.data;
+                })
+                .catch(function(error) {
+                    console.log(error);
+                    $rootScope.alertsMsg.error('Error while retrieving new clients graphs.');
+                });
+        }
     }
 
 })();
