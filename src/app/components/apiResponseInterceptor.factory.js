b833ff493e7b0fef28fa54f9c39293dc9d84dd71
Tuxity <kevin.darcel@gmail.com>
2015-11-29 19:17:55 -0600

    Add interceptor to remove top level field 'data' from response

---
+++ src/app/components/apiResponseInterceptor.factory.js
@@ -0,0 +1,28 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('sheaker')
+        .factory('apiResponseInterceptor', apiResponseInterceptor);
+
+    /** @ngInject */
+    function apiResponseInterceptor(BACKEND_URL) {
+        var service = {
+            response: removeDataField
+        };
+
+        return service;
+        ////////////
+
+        function removeDataField(response) {
+            // For responses from the api we "rewrite" the content to remove the top level field "data"
+            // See http://jsonapi.org/format/#document-top-level
+            if (response.config.url.indexOf(BACKEND_URL) !== -1) {
+                response.data = response.data.data;
+            }
+
+            return response;
+        }
+    }
+
+})();
