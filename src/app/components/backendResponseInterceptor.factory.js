d8cb9f0c3294f1236ad0462ffa650cb23e99f330
Tuxity <kevin.darcel@gmail.com>
2015-11-29 19:55:11 -0600

    Also rewrite api response in case of errors

--- src/app/components/backendResponseInterceptor.factory.js
+++ src/app/components/backendResponseInterceptor.factory.js
@@ -15,10 +15,15 @@
         ////////////
 
         function removeDataField(response) {
-            // For responses from the api we "rewrite" the content to remove the top level field "data"
+            // For responses from the api we "rewrite" the content to remove top level fields "data" and "errors"
             // See http://jsonapi.org/format/#document-top-level
             if (response.config.url.indexOf(BACKEND_URL) !== -1) {
-                response.data = response.data.data;
+                if (angular.isDefined(response.data.data)) {
+                    response.data = response.data.data;
+                }
+                if (angular.isDefined(response.data.errors)) {
+                    response.data = response.data.errors;
+                }
             }
 
             return response;
